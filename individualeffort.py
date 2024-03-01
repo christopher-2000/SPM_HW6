e9edfe5a857a4d384093be9470722b4cf060d97e
neharajeeva <rajen01@pfw.edu>
2024-02-29 20:36:19 -0500

    main

--- individualeffort.py
+++ individualeffort.py
@@ -1,5 +1,7 @@
-def calculate_individual_effort_capacity(member_data, sprint_days):
+from read_team_data import read_team_data_from_csv
+def calculate_individual_effort_capacity(csv_file_path, sprint_days):
     
+    member_data = read_team_data_from_csv(csv_file_path)
     pto_hours = member_data['PTO Hours']
     ceremony_hours = member_data['Ceremony Hours']
     available_hours_per_day = member_data['Available Hours/Day']
--- teameffort.py
+++ teameffort.py
@@ -1,5 +1,7 @@
-def calculate_team_effort_capacity(team_data, sprint_days):
-
+from read_team_data import read_team_data_from_csv
+def calculate_team_effort_capacity(csv_file_path, sprint_days):
+    
+    team_data = read_team_data_from_csv(csv_file_path)
     team_data['Total Available Hours'] = (sprint_days * team_data['Available Hours/Day']) - team_data['PTO Hours'] - (sprint_days * team_data['Ceremony Hours'])
     total_effort_hours = team_data['Total Available Hours'].sum()
     return total_effort_hours
